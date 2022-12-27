fc0cbbfa4fcdda1fff37d80a8d7011df403e68c1
Dimasiys228 <118446091+Dimasiys228@users.noreply.github.com>
2022-12-27 18:41:39 +0300

    Add files via upload

---
+++ src/gift/Ingred.java
@@ -0,0 +1,24 @@
+package gift;
+
+public class Ingred {
+
+    public enum ingred{
+        Figures(8, 0),
+        Chokolate(10, 0),
+        Lollipops(3, 0),
+        ChokCereals(4, 0),
+        Choupa(1, 0),
+        Candies(9, 0),
+        Candies2(5, 0);
+   private int priceOfIngred;
+   private int amountOfIngred;
+ingred(int priceOfIngred,int amountOfIngred){
+this.priceOfIngred = priceOfIngred;
+
+
+}
+
+
+    }
+
+}
