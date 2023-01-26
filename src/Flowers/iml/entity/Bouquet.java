1e9cacff3e252919530de1294245fb84bed29cb7
Dimasiys228 <118446091+Dimasiys228@users.noreply.github.com>
2023-01-26 16:57:36 +0300

    Add files via upload

---
+++ src/Flowers/iml/entity/Bouquet.java
@@ -0,0 +1,16 @@
+package Flowers.iml.entity;
+
+import java.util.*;
+
+public class Bouquet {
+
+private List<Flower> bouquet = new ArrayList<>();
+    public void addFlower(Flower flower) {
+        bouquet.add(flower);
+    }
+
+
+
+}
+
+
---
+++ src/Flowers/iml/entity/Flower.java
@@ -0,0 +1,94 @@
+package Flowers.iml.entity;
+
+import java.util.Objects;
+
+
+public class Flower {
+    static final double MAX_COST = 100;
+    static final double MIN_COST = 1;
+
+    static final double MAX_LENGHT = 10;
+    static final double MIN_LENGHT = 1;
+
+    static final double MAX_WEIGHT = 15;
+    static final double MIN_WEIGHT = 1;
+
+    static final int MAX_FRESH = 10;
+    static final int MIN_FRESH = 0;
+    private double cost;
+    private double lenght;
+    private double weight;
+    private int fresh;
+
+
+    public Flower(double cost, double lenght, double weight, int fresh) {
+        this.cost = cost;
+        this.lenght = lenght;
+        this.fresh = fresh;
+        this.weight = weight;
+    }
+
+    public double getCost() {
+
+        return cost;
+    }
+
+    public double getLenght() {
+
+        return lenght;
+    }
+
+    public double getWeight() {
+
+        return weight;
+    }
+
+    public int getFresh() {
+
+        return fresh;
+    }
+
+    public void setCost(double cost) {
+
+        this.cost = cost;
+    }
+
+    public void setLenght(double lenght) {
+
+        this.lenght = lenght;
+    }
+
+    public void setWeight(double weight)
+    {
+        this.weight = weight;
+    }
+
+    public void setFresh(int fresh) {
+
+        this.fresh = fresh;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flower flower = (Flower) o;
+        return Double.compare(flower.cost, cost) == 0 && Double.compare(flower.lenght, lenght) == 0 && Double.compare(flower.weight, weight) == 0 && fresh == flower.fresh;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(cost, lenght, weight, fresh);
+    }
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "cost=" + cost +
+                ", lenght=" + lenght +
+                ", weight=" + weight +
+                ", fresh=" + fresh +
+                '}';
+    }
+}
