870c2d9560fbfa26af799ba7906bd85bf7aed4de
Dimasiys228 <118446091+Dimasiys228@users.noreply.github.com>
2023-01-26 16:57:22 +0300

    Delete src/src/Flowers/iml directory

--- src/src/Flowers/iml/Astrov.java
+++
@@ -1,9 +0,0 @@
-package Flowers.iml;
-
-public class Astrov extends Flower{
-    char color;
-    public Astrov(double cost, double lenght, double weight,int fresh,char color)
-    {
-        super(cost, lenght, weight,fresh);
-    }
-}
--- src/src/Flowers/iml/Bob.java
+++
@@ -1,9 +0,0 @@
-package Flowers.iml;
-
-public class Bob extends Flower {
-    boolean desiccation;
-
-    public Bob(double cost, double lenght, double weight,int fresh,boolean desiccation) {
-        super(cost, lenght, weight,fresh);
-    }
-}
