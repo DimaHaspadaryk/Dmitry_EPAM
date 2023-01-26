038554e416f3341711bb0e2898407e96e7371c2c
Dimasiys228 <118446091+Dimasiys228@users.noreply.github.com>
2023-01-26 16:57:58 +0300

    Delete Flowers/iml directory

--- Flowers/iml/Astrov.java
+++
@@ -1,7 +0,0 @@
-package Flowers.iml;
-
-public class Astrov extends Flower{
-    public Astrov(double cost, double lenght, double weight,int fresh) {
-        super(cost, lenght, weight,fresh);
-    }
-}
